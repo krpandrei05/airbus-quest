af81f5a8d3a2cf1255494f2b28607ea493f8bdc4
Andrei Carp <andrei.krp@gmail.com>
2026-02-15 20:57:11 +0200

    feat(location): implement GPS tracking with Bundle data passing
    Signed-off-by: Andrei Carp <andrei.krp@gmail.com>

--- app/src/main/java/com/example/project_mad/ThirdActivity.kt
+++ app/src/main/java/com/example/project_mad/ThirdActivity.kt
@@ -1,9 +1,11 @@
 package com.example.project_mad
 
 import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,6 +32,15 @@ class ThirdActivity : AppCompatActivity() {
             Log.d(ThirdTag, "Click: Going to SecondActivity.")
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+        }
+
+        val bundle = intent.getBundleExtra("locationBundle")
+        val location: Location? = bundle?.getParcelable("location")
+        if (location != null) {
+            val tvLocation: TextView = findViewById(R.id.tvLocation)
+            tvLocation.text = "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
+
+            Log.i(ThirdTag, "onCreate: Location received from Bundle [${location.latitude}] [${location.longitude}]")
         }
     }
 }
